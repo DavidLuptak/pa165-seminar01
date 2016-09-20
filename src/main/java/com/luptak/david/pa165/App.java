cc7bd2ba7580b94985297ec601c776af67a7b76c
Dávid Lupták <luptak.david@gmail.com>
2016-09-20 18:56:44 +0200

    [ADD] Initial commit.

---
+++ src/main/java/com/luptak/david/pa165/App.java
@@ -0,0 +1,13 @@
+package com.luptak.david.pa165;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello PA165!" );
+    }
+}
